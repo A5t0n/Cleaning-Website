5ae61dc1863fc4015c61f4180073c0d670a2c1ae
A5t0n <123136923+A5t0n@users.noreply.github.com>
2025-06-26 03:42:33 +0000

    deleted unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
-import NavBar from './Components/Navbar/Navbar'
-import Body from './Components/Body/Body'
-import Footer from './Components/Footer/Footer'
+
 
 function App() {
   return (
