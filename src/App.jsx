efe52134b1b855cfb687a767f62059f8303f5d8f
A5t0n <123136923+A5t0n@users.noreply.github.com>
2025-06-26 03:38:52 +0000

    minor change in app.js for changing website to multi page

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
 import NavBar from './Components/Navbar/Navbar'
 import Body from './Components/Body/Body'
 import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
-    <div className='App'>
-    <div className='components'>
-    <NavBar/>
-    <Body/>
-    <Footer/>
-    </div>
-   
-    </div>  
+     <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Services/>}/>
+      <Route path="portfolio" element={<Contact/>}/>
+      </Route>      
+    </Routes>
+    </>
   )
 }
 
