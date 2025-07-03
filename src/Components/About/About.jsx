9fed8c8463fb738d927fff2b20e2c7b96208fdcd
A5t0n <123136923+A5t0n@users.noreply.github.com>
2025-07-03 03:35:38 +0000

    fixed an issue with about us heading

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,11 +9,12 @@ function About () {
 
     return (
         <>
-            <h2 >About Us</h2>
-                <div className="About-page-container">
-                    <section id="about" className="about-section">
-                        <div className="about-content">
-                        
+            <div className="About-page-container">
+
+                <h2 className='about-heading'>About Us</h2>
+                
+                <section id="about" className="about-section">
+                    <div className="about-content">   
                         <p>
                             At Spotless Touch Inc, we are committed to delivering top-quality cleaning services tailored to fit your needs. Whether you're looking for a one-time deep clean or ongoing cleaning contracts, we’ve got you covered with flexible, reliable, and professional service.
                         </p>
@@ -23,10 +24,9 @@ function About () {
                         <p>
                             From residential and commercial cleaning to event, office, and even kitchen and bathroom-specific services, our offerings are designed to suit every lifestyle and environment. Looking ahead, we plan to expand into more specialized cleaning areas to better serve our community.
                         </p>
-                        </div>
-                    </section>
-                <div id='services'></div>
-                </div>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
